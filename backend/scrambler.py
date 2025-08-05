ddf0f302d2bf0674436e50f18959fed694283212
Ross Burnett <rosswburnett313@gmail.com>
2025-08-06 00:26:07 +0100

    Delete scrambler.py

--- backend/scrambler.py
+++
@@ -1,4 +0,0 @@
-from pyTwistyScrambler import scrambler333, scrambler222, scrambler444,\
-	megaminxScrambler, squareOneScrambler, ftoScrambler, rexScrambler
-
-print(scrambler333.get_WCA_scramble())
